b65d6c2efab888c53f0297cbd397534c279a741c
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2021-01-06 18:19:09 -0300

    new get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
 
 const mysqlConnection = require('../database');
 router.get('/', (req, res) => {
-mysqlConnection.query('SELECT * FROM pregunta',(err,rows))
+mysqlConnection.query('SELECT * FROM pregunta',(err,rows,fields)=>{
+    if (!err){
+        res.json(rows);
+    }else{
+        console.log("vjfivfjbfkngb");
+    }
+})
 });
 
 module.exports = router;
